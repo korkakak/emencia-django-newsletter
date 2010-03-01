ea0c158dc64b64507385e1cf1dc742a1a2e2ddf7
Korkakakis Nikos <korkakak@gmail.com>
2010-03-01 14:45:16 +0200

    Added views and url necessary to handle the export.

--- emencia/django/newsletter/urls/__init__.py
+++ emencia/django/newsletter/urls/__init__.py
@@ -2,8 +2,11 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',                       
-                       (r'^mailing/', include('emencia.django.newsletter.urls.mailing_list')),
-                       (r'^tracking/', include('emencia.django.newsletter.urls.tracking')),
-                       (r'^statistics/', include('emencia.django.newsletter.urls.statistics')),
-                       (r'^', include('emencia.django.newsletter.urls.newsletter')),
-                       )
+                    url (r'^export_csv/$',
+                    view='emencia.django.newsletter.views.generate_csv',
+                    name='generate_csv',),
+                    (r'^mailing/', include('emencia.django.newsletter.urls.mailing_list')),
+                    (r'^tracking/', include('emencia.django.newsletter.urls.tracking')),
+                    (r'^statistics/', include('emencia.django.newsletter.urls.statistics')),
+                    (r'^', include('emencia.django.newsletter.urls.newsletter')),
+                    )
--- emencia/django/newsletter/views/__init__.py
+++ emencia/django/newsletter/views/__init__.py
@@ -1,1 +1,21 @@
+from django.contrib.admin.views.decorators import staff_member_required
+from django.db.models import get_model
 
+from emencia.django.newsletter.models import Contact
+from emencia.django.newsletter.utils import export_csv
+
+
+@staff_member_required
+def generate_csv(request, model=Contact, data=None):
+    """
+    Wrap all users in emencia.django.newsletter.Contact and send them to export_csv
+
+    This should export all registered contacts to a csv.
+    http://www.djangosnippets.org/snippets/1151/
+    """
+    if not data:
+        data = model._default_manager.filter(subscriber=True)
+
+    if len(data) == 0:
+        data = [["no subscriptions"],]
+    return export_csv.ExcelResponse(data)
